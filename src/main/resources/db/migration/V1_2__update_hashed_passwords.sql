af288d9e3913793f042d7ef29c42f83a5b68d4e8
Ignacio Veiga <ignacio.veiga29@gmail.com>
2024-06-26 20:03:46 -0300

    Passwords for demo users

---
+++ src/main/resources/db/migration/V1_2__update_hashed_passwords.sql
@@ -0,0 +1,11 @@
+UPDATE user_credentials
+	SET password_hash='$2a$10$w5qXpYFu8erYyxGQP7VnSuUK/427ssz3NJ0dwmqvpQ5ogOErLl2wC'
+	WHERE id=1;
+
+UPDATE user_credentials
+	SET password_hash='$2a$10$yM8Eqb9DLyKMTlcCEnSDROt/il3RhBelJi4wa4RicvrqUY1zFu3s2'
+	WHERE id=2;
+
+UPDATE user_credentials
+	SET password_hash='$2a$10$j9S5n6Tcv7ZVdn5xAcLwD..ggfdVftNcBKeeFLghoIMxG8eFvDsti'
+	WHERE id=3;
