94e862e925b3a4992f7a272d9ecb075960b81c33
Ignacio Veiga <ignacio.veiga29@gmail.com>
2024-11-30 01:24:16 -0300

    RememberMe not used

--- src/main/java/com/backend/situ/model/LoginDTO.java
+++ src/main/java/com/backend/situ/model/LoginDTO.java
@@ -2,6 +2,5 @@ package com.backend.situ.model;
 
 public record LoginDTO(
         String email,
-        String password,
-        Boolean rememberMe // may be unnecessary
+        String password
 ) { }
